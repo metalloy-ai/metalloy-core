3e3c9c46d3b2b8a79047882ad9988d9b490df040
tuan882612 <tuan882612@gmail.com>
2023-04-18 18:45:10 -0500

    feat: implemented a response wrapper

--- pkg/response/base.go
+++ pkg/response/base.go
@@ -1,7 +1,28 @@
 package response
 
-type BaseResponse struct {
-	Code 	int    `json:"code"`
-	Message string `json:"message"`
-	Body 	any    `json:"body"`
+import (
+	"net/http"
+	// "strconv"
+
+	"github.com/uptrace/bunrouter"
+)
+
+type Response struct {
+	Code 	int 		`json:"code"`
+	Data 	interface{} `json:"body"`
+	Message string		`json:"message"`
+}
+
+func InitBody(data interface{}, code int, message string) Response {
+	return Response{
+		Code: code,
+		Message: message,
+		Data: data,
+	}
+}
+
+func WrapRes(w http.ResponseWriter, body Response) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(body.Code)
+	_ = bunrouter.JSON(w, body)
 }
