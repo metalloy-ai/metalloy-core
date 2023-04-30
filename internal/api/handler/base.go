055639a6c1574da5ea0c596806c976f9e5e3e716
tuan882612 <tuan882612@gmail.com>
2023-04-30 10:00:01 -0500

    feat: changed to response wrapper to using pointers to increase memory/runtime performance

--- internal/api/handler/base.go
+++ internal/api/handler/base.go
@@ -3,14 +3,14 @@ package handler
 import (
 	"net/http"
 	"strconv"
-	
+
 	"metalloyCore/internal/config"
 	"metalloyCore/pkg/response"
 )
 
 func DefaultHandler(w http.ResponseWriter, req *http.Request) {
-	body := response.InitRes(http.StatusOK, "Welcome to Logiflow Core", nil)
-	response.WrapRes(w, body)
+	body := *response.InitRes(http.StatusOK, "Welcome to Logiflow Core", nil)
+	response.WrapRes(w, &body)
 }
 
 func HealthHandler(cfg config.Setting) http.HandlerFunc {
@@ -21,7 +21,7 @@ func HealthHandler(cfg config.Setting) http.HandlerFunc {
 			"host":    cfg.Host,
 			"port":    strconv.Itoa(cfg.Port),
 		}
-		body := response.InitRes(http.StatusOK, "", data)
-		response.WrapRes(w, body)
+		body := *response.InitRes(http.StatusOK, "", data)
+		response.WrapRes(w, &body)
 	}
 }
--- pkg/response/base.go
+++ pkg/response/base.go
@@ -12,15 +12,15 @@ type Response struct {
 	Data 	interface{} `json:"body"`
 }
 
-func InitRes(code int, message string, data interface{}) Response {
-	return Response{
+func InitRes(code int, message string, data interface{}) *Response {
+	return &Response{
 		Code: code,
 		Message: message,
 		Data: data,
 	}
 }
 
-func WrapRes(w http.ResponseWriter, body Response) {
+func WrapRes(w http.ResponseWriter, body *Response) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(body.Code)
 	_ = bunrouter.JSON(w, body)
