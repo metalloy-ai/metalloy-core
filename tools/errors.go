f33316982a2aa0e3d8eb1a7f672581efc9f737ce
tuan882612 <tuan882612@gmail.com>
2023-04-30 09:39:20 -0500

    feat: add new error type for all users handler

--- tools/errors.go
+++ tools/errors.go
@@ -1,8 +1,11 @@
 package tools
 
 import (
+	"fmt"
 	"net/http"
-	
+
+	"github.com/jackc/pgx/v5"
+
 	"metalloyCore/pkg/response"
 )
 
@@ -11,15 +14,20 @@ type ErrInvalidReqBody struct{}
 type ErrMissingParams struct{}
 type ErrUserNotFound struct{}
 type ErrUserAlreadyExist struct{}
+type ErrFailedUsers struct{ 
+	Users map[string]interface{}
+	FailedUsers []pgx.Row 
+}
 
-func (e ErrInvalidCredentials) Error() string {return "invalid credentials"}
-func (e ErrInvalidReqBody) Error() string {return "invalid request body"}
-func (e ErrMissingParams) Error() string {return "missing params"}
-func (e ErrUserNotFound) Error() string {return "user not found"}
-func (e ErrUserAlreadyExist) Error() string {return "user already exists"}
+func (e ErrInvalidCredentials) Error() string { return "invalid credentials" }
+func (e ErrInvalidReqBody) Error() string     { return "invalid request body" }
+func (e ErrMissingParams) Error() string      { return "missing params" }
+func (e ErrUserNotFound) Error() string       { return "user not found" }
+func (e ErrUserAlreadyExist) Error() string   { return "user already exists" }
+func (e ErrFailedUsers) Error() string        { return fmt.Sprintf("%d users failed to load", len(e.FailedUsers)) }
 
 func HandleError(err error, w http.ResponseWriter) bool {
-	switch err.(type) {
+	switch err := err.(type) {
 	case ErrInvalidCredentials:
 		body := response.InitRes(http.StatusUnauthorized, "Unauthorized: login failed, invalid username or password", nil)
 		response.WrapRes(w, body)
@@ -34,6 +42,9 @@ func HandleError(err error, w http.ResponseWriter) bool {
 		response.WrapRes(w, body)
 	case ErrMissingParams:
 		body := response.InitRes(http.StatusBadRequest, "Bad request: Missing or Empty parameter", nil)
+		response.WrapRes(w, body)
+	case ErrFailedUsers:
+		body := response.InitRes(http.StatusInternalServerError, err.Error(), err.Users)
 		response.WrapRes(w, body)
 	case nil:
 		return true
