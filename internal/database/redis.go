080378b7e4d7a3233eb1ab2148222b771e7a43f8
tuan882612 <tuan882612@gmail.com>
2023-05-13 19:17:39 -0500

    refactor: implemented singleton pattern for redis

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -1,16 +1,24 @@
 package database
 
 import (
-	"metalloyCore/internal/config"
+	"sync"
 
 	"github.com/go-redis/redis"
+
+	"metalloyCore/internal/config"
 )
 
+var conn *redis.Client
+var once sync.Once
+
 func GetRedisClient(cfg config.Setting) *redis.Client {
-	conn := redis.NewClient(&redis.Options{
-		Addr:     cfg.REDIS_URL,
-		Password: cfg.REDIS_PWS,
-		DB:       0,
+	once.Do(func() {
+		conn = redis.NewClient(&redis.Options{
+			Addr:     cfg.REDIS_URL,
+			Password: cfg.REDIS_PWS,
+			DB:       0,
+		})
 	})
+		
 	return conn
 }
