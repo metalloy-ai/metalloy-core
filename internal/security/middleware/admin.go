d3265864bfb156f690ca858ee601409f547cad41
tuan882612 <tuan882612@gmail.com>
2023-05-20 20:06:26 -0500

    feat: start on admin middlware

--- internal/security/middleware/admin.go
+++ internal/security/middleware/admin.go
@@ -1,1 +1,13 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/uptrace/bunrouter"
+)
+
+func AdminMiddleware(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
+	return func(w http.ResponseWriter, req bunrouter.Request) error {
+		return next(w, req)
+	}
+}
